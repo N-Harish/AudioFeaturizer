f399e6c49db30ea164eb7c45d9d45de7695b455c
N-Harish <harishnatarajan24@gmail.com>
2020-11-11 18:21:53 +0530

    Added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import setuptools
+
+
+setup(
+    name="realpython-reader",
+    version="1.0.0",
+    description="Takes audio as input and returns computed features as a dataframe",
+    url="https://github.com/N-Harish/AudioFeaturizer",
+    author_email="harishnatarajan24@gmail.com",
+    author='Harish Natarajan',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['librosa', "pandas", "numpy", "matplotlib"],
+    python_requires='>=3.7'
+)
